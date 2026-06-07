244f787983ba5e909489bc29b7ebd699213af39a
JulianTettamanti <tettamantijulian@gmail.com>
2026-06-07 11:44:10 -0300

    Render anda por favor

--- src/main/java/ar/edu/utn/dds/k3003/app/Application.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/Application.java
@@ -2,8 +2,10 @@ package ar.edu.utn.dds.k3003.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "ar.edu.utn.dds.k3003")
+@EnableJpaRepositories(basePackages = "ar.edu.utn.dds.k3003.repositories")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
